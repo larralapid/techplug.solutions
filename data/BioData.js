3a8246a9727daff78facc5a2a728859d8d01d0ac
larra <larralapid@gmail.com>
2025-04-23 20:34:55 -0400

    Initial commit

---
+++ data/BioData.js
@@ -0,0 +1,25 @@
+// date: 1 Aug, 2022
+const bioData = [
+    {
+        name: 'Apple Shortcuts Gallery',
+        username: '',
+        url: 'https://larralapid.dev',
+        titleImg: false,
+        avatar: '/avatar.png',
+        nftAvatar: true,
+        description: ' ',
+        descShow: true,
+        subdesc: '',
+        subdescShow: true,
+        newProductUrl: 'https://3dicons.co',
+        newProduct: true,
+        author: '',
+        authorURL: 'tiktok.com/@tilatechila',
+        footerText: '🪐'
+    },
+];
+
+export default bioData;
+
+
+
