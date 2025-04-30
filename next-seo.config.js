4899bc94aa110f01a6eb0d6fa2d9b779712882ac
larra <larralapid@gmail.com>
2025-04-30 14:12:59 -0400

    Update cover image URL to point to the new brain graph image in SEO configuration

--- next-seo.config.js
+++ next-seo.config.js
@@ -11,7 +11,7 @@ export default {
             {
                 width: 1200,
                 height: 630,
-                url: `https://vjy.me/preview.jpg`,
+                url: `https://github.com/larralapid/techplug.solutions/blob/main/public/brain%20graph.png?raw=true`,
             },
         ],
         site_name: 'techplug.solutions',
